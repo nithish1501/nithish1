620269fec2d8c3d6f1f6dfa37e067e71a7ac4cb1
nithish1501 <nithishanish2007@gmail.com>
2026-02-28 19:25:31 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+c = Circle(5)
+print("Area:", c.area())
